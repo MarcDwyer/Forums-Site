58178bdf52898a28018dc097cb447449d9e19289
Marc Dwyer <envdia@gmail.com>
2018-10-02 19:49:17 -0500

    Deploying...

--- forumposts/src/reducers/reducer_login.js
+++
@@ -1,9 +0,0 @@
-import {USER} from '../actions/index';
-
-export default function(state = null, action) {
-    switch (action.type) {
-        case USER:
-        return action.payload.data
-    }
-    return state;
-}
